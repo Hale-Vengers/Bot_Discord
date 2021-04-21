43600be16c11baedf731d4d622f45a08513b5382
agerard57 <agerard57@protonmail.com>
2021-04-21 21:16:33 +0200

    repaired the menu

--- bot/__main__.py
+++ bot/__main__.py
@@ -46,9 +46,54 @@ async def on_message(message):
         
     await bot.process_commands(message)
 
+@bot.command()
+async def help(ctx):
+    await ctx.send("La commande d'aide est $aide :)")
+
+@bot.command(pass_context=True)
+async def aide(ctx):
+    author= ctx.message.author
+
+    embed = discord.Embed(
+        colour = discord.Colour.purple(),
+        title = "--- Page d'aide ---"
+        )
+
+    embed.set_thumbnail(url = "https://media.discordapp.net/attachments/813145774320910359/824010122581114970/received_187979142621558.jpeg?width=300&height=300")
+
+    embed.add_field(name = "____________________\n------> Bonus :", value="Commandes bonus", inline = False)
+    embed.add_field(name = "$aide", value = "Envoi la page que vous voyez actuellement", inline = False)
+    embed.add_field(name = "$bonbot", value = "Remercie le bot pour ses loyaux services", inline = False)
+    embed.add_field(name = "$dodo", value = "Souhaite une bonne nuit", inline = False)
+    embed.add_field(name = "$hayato #", value = "Invoque Hayato # fois", inline = False)
+    embed.add_field(name = "$icedblue", value = "Réponds à la question \"quelle est la meilleure des bières ?\"", inline = False)
+
+    embed.add_field(name = "____________________\n------> Empire :", value="Commandes dédiés au rappeur EMPIRE", inline = False)
+    embed.add_field(name = "$impardonnable", value = "Sors une quote de son son Impardonnable", inline = False)
+    embed.add_field(name = "$lyricsimpardonnable", value = "Envoi en MP les lyrics d'Impardonnable.", inline = False)
+
+    embed.add_field(name = "____________________\n------> Personnalisés :", value="Commandes dédiés aux Hale-Vengers", inline = False)
+    embed.add_field(name = "$fantomas", value = "Deviens FANTOMAS", inline = False)
+    embed.add_field(name = "$hale", value = "Juste Hale.", inline = False)
+    embed.add_field(name = "$hoothoot #", value = "HOOT-HOOT (# = le nombre de fois ou il doit apparaître", inline = False)
+    embed.add_field(name = "$raph", value = "?", inline = False)
+
+    embed.add_field(name = "____________________\n------> Signes :", value="Commandes liés au système de signes", inline = False)
+    embed.add_field(name = "$addsigne ######", value = "Cette commande permet d'ajouter une phrase (ici ######) dans la liste de signes.", inline = False)
+    embed.add_field(name = "$listesignes", value = "Cette commande permet de lister les différentes phrases", inline = False)
+    embed.add_field(name = "$donneznousunsigne", value = "Cette commande donne un signe au hasard parmi la liste.", inline = False)
+
+    embed.add_field(name = "____________________\n------> Textuels :", value="Commandes dédiés au chat", inline = False)
+    embed.add_field(name = "$clear #", value = "Supprime # message(s) du chat", inline = False)
+
+    embed.add_field(name = "____________________\n------> Vocal :", value="Commandes liés aux interactions vocales", inline = False)
+    embed.add_field(name = "$viens", value = "Connecte le bot dans le canal vocal", inline = False)
+    embed.add_field(name = "$cassetoi", value = "Déconnecte le bot du le canal vocal", inline = False)
+    embed.add_field(name = "$lost", value = "Joue la meilleure musique qui existe", inline = False)
 
 
-
+    await ctx.send("La liste des commandes à été envoyé par MP !")
+    await author.send(embed=embed)
 
 @bot.command()
 async def clear(ctx, nb_del):
@@ -62,36 +107,6 @@ async def clear(ctx, nb_del):
     time.sleep(3)
     
     await botmsg.delete()
-
-@bot.command()
-async def impardonnable(ctx):
-    r=open("saves/impardonnable.txt","r")
-    m=r.readlines()
-    l=[]
-    for i in range(0,len(m)-1):
-        x=m[i]
-        z=len(x)
-        a=x[:z-1]
-        l.append(a)
-    l.append(m[i+1])
-    o=random.choice(l)
-    await ctx.send("\"***" + o + "***\" Empire, 2019 ")
-    r.close()
-
-@bot.command(pass_context=True)
-async def lyricsimpardonnable(ctx):
-    author= ctx.message.author
-    r = open("saves/impardonnable.txt", "r")
-    l_msg= discord.Embed(
-        title="Empire - Impardonnable (2019)",
-        description = ' '.join(r.readlines()),
-        colour = discord.Colour.purple()
-        )
-    
-    await ctx.send("Les lyrics ont étés envoyés par MP !")
-    await author.send(embed = l_msg)
-    
-    r.close()
 
 @bot.command()
 async def bonbot(ctx):
